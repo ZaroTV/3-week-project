819dcea61f6ac115850b8addc130c97db0fb2469
mista makzim <56073641+ZaroTV@users.noreply.github.com>
2020-04-22 18:57:06 +0200

    Added summoning Hall and underling

---
+++ LDM-3-Week/Assets/Scripts/SummoningHall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.Apple;
+
+public class SummoningHall : MonoBehaviour
+{
+    public int essence = 100; // The resource for summoning underlings
+    public int costSum = 20; //Cost for summoning 1 underling
+    public GameObject underling;
+    void OnMouseDown()
+    {
+        if (essence >= costSum)
+        {
+            Instantiate(underling, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+}
